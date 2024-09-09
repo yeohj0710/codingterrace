f2ec3dc2431aaa7644121663fdd3521b58933f3c
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-09-09 20:33:16 +0900

    2409092033

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,10 +1,52 @@
-async function getPosts() {}
+import db from "@/lib/db";
+import Link from "next/link";
+
+async function getPosts() {
+  const posts = await db.post.findMany({
+    select: {
+      idx: true,
+      user: true,
+      category: true,
+      title: true,
+      content: true,
+      created_at: true,
+    },
+  });
+  return posts;
+}
 
 export default async function Board() {
   const posts = await getPosts();
   return (
-    <div className="flex flex-col h-[150vh] bg-white p-5 gap-2 relative">
-      <span className="font-bold">게시판</span>
+    <div className="flex flex-col items-center pt-8">
+      <div className="flex flex-col w-[640px] lg:w-1/2 bg-white p-5 gap-3 shadow-lg rounded-lg">
+        <span className="text-2xl font-bold text-gray-800 mt-4">게시판</span>
+        <hr className="border-gray-300 my-4" />
+        {posts.map((post) => (
+          <Link
+            href={`/board/${post.idx}`}
+            key={post.idx}
+            className="flex flex-col w-full bg-gray-100 p-4 rounded-lg shadow-sm mb-2"
+          >
+            <span className="text-lg font-semibold text-gray-700">
+              {post.title}
+            </span>
+            <p className="text-sm text-gray-500 mt-2">{post.content}</p>
+            <div className="flex justify-between items-center mt-4 text-gray-600 text-xs">
+              <span>{post.user.nickname}</span>
+              <span>
+                {new Date(post.created_at).toLocaleString("ko-KR", {
+                  year: "numeric",
+                  month: "2-digit",
+                  day: "2-digit",
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </span>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,33 +9,35 @@ import Link from "next/link";
 export default function Login() {
   const [state, action] = useFormState(login, null);
   return (
-    <div className="flex flex-col gap-6 py-8 px-6">
-      <div className="flex flex-col gap-2">
-        <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">아이디와 비밀번호로 로그인해 주세요.</h2>
-      </div>
-      <form action={action} className="flex flex-col gap-3">
-        <FormInput
-          name="id"
-          type="text"
-          placeholder="아이디"
-          required
-          errors={state?.fieldErrors.id}
-        />
-        <FormInput
-          name="password"
-          type="password"
-          placeholder="비밀번호"
-          required
-          errors={state?.fieldErrors.password}
-        />
-        <FormButton text="로그인" />
-      </form>
-      <div className="flex flex-row gap-2">
-        <span>계정이 없으신가요?</span>
-        <Link href="/join" className="font-bold">
-          계정 만들기 &rarr;
-        </Link>
+    <div className="flex flex-col items-center">
+      <div className="flex flex-col w-[512px] gap-6 py-8 px-6">
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl">안녕하세요!</h1>
+          <h2 className="text-xl">아이디와 비밀번호로 로그인해 주세요.</h2>
+        </div>
+        <form action={action} className="flex flex-col gap-3">
+          <FormInput
+            name="id"
+            type="text"
+            placeholder="아이디"
+            required
+            errors={state?.fieldErrors.id}
+          />
+          <FormInput
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+            required
+            errors={state?.fieldErrors.password}
+          />
+          <FormButton text="로그인" />
+        </form>
+        <div className="flex flex-row gap-2">
+          <span>계정이 없으신가요?</span>
+          <Link href="/join" className="font-bold">
+            계정 만들기 &rarr;
+          </Link>
+        </div>
       </div>
     </div>
   );
