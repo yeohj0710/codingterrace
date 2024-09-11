6197e9f49b368d6adbf8dc741acd3ed5ac3f7761
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-09-11 14:58:30 +0900

    2409111458

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,25 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "코딩테라스",
-  description: "코딩테라스",
+  description:
+    "코딩테라스에서 NextJS를 이용해 구현한 다양한 기능들을 체험해 보세요!",
   icons: {
     icon: "/icon.png",
+  },
+  openGraph: {
+    title: "코딩테라스",
+    description:
+      "코딩테라스에서 NextJS를 이용해 구현한 다양한 기능들을 체험해 보세요!",
+    url: "https://codingterrace.com/",
+    images: [
+      {
+        url: "https://avatars.githubusercontent.com/u/93759367?v=4",
+        width: 800,
+        height: 800,
+        alt: "코딩테라스",
+      },
+    ],
+    siteName: "코딩테라스",
   },
 };
 
