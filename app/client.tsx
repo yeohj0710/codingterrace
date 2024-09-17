54a4acc261f9a43ce3911b67f53d0f8076541ad5
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-09-17 19:08:13 +0900

    2409171908

--- app/client.tsx
+++ app/client.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-export default function openExternalInKakao() {
+export default function OpenExternalInKakao() {
   useEffect(() => {
     openExternalInKakao();
   }, []);
