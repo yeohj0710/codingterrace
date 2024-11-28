a8471752fee5bbef850517f7b3f0bbe10ec93899
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-11-29 00:46:38 +0900

    2411290046

--- lib/subscription.ts
+++ lib/subscription.ts
@@ -3,26 +3,10 @@ import db from "./db";
 export const findSubscription = async (
   endpoint: string,
   type: string,
-  postId: number | null,
-  commentId: number | null
+  postId: number | null = null,
+  commentId: number | null = null
 ) => {
   return await db.subscription.findFirst({
-    where: {
-      endpoint,
-      type,
-      postId: postId ?? null,
-      commentId: commentId ?? null,
-    },
-  });
-};
-
-export const deleteSubscription = async (
-  endpoint: string,
-  type: string,
-  postId: number | null,
-  commentId: number | null
-) => {
-  return await db.subscription.deleteMany({
     where: {
       endpoint,
       type,
